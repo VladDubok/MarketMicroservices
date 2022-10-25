e6c4eeabcf31ae526cf4f0ec3eecb9d5005a103a
VladDubok <vladislav.dubok@itomy.ch>
2022-10-25 20:45:05 +0300

    refactored project and added fluent migration

---
+++ Market/Products.Data/Migrations/CreateProductTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace Products.Data.Migrations
+{
+    [Migration(1)]
+    internal class CreateProductTable : Migration 
+    {
+        public override void Up()
+        {
+            Create.Table("Products")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Name").AsString().NotNullable()
+                .WithColumn("Price").AsDouble().NotNullable()
+                .WithColumn("AmountLeft").AsDouble().NotNullable()
+                .WithColumn("UpdatedAt").AsDateTime().Nullable()
+                .WithColumn("CreatedAt").AsDateTime().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Products");
+        }
+    }
+}
